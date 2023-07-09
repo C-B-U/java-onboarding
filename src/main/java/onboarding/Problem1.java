d9916e9e4f841db2183af13fd9485d0c55c17740
Jiyoon Kim <rlawldbs0911@naver.com>
2023-07-09 18:30:41 +0900

    feat: problem1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,46 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        System.out.println(pobi+ " "+ crong);
+        int pobiRight = pobi.get(0);
+        int pobiLeft = pobi.get(1);
+        int crongRight = crong.get(0);
+        int crongLeft = crong.get(1);
+
+        return 0;
+    }
+
+    // 더하기
+    private static int arrAdd(int number) {
+        int sum = 0;
+        while (number > 0) {
+            sum += number%10;
+            number /= 10;
+        }
+
+        return sum;
+    }
+
+    // 곱하기
+    private static int arrMul(int number) {
+        int sum = 0;
+        while (number > 0) {
+            sum *= number%10;
+            number /= 10;
+        }
+
+        return sum;
+    }
+
+    private Boolean checkMax(int right, int left) {
+
+        int addMax = 0;
+        int mulMax = 1;
+
+        if (arrAdd(right) > arrAdd(left)) {
+            addMax = 0;
+        }
+
+        return true;
     }
 }
