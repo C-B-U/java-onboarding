743b9ded05d3f8a82543e602c3c5147783c8299b
Jiyoon Kim <rlawldbs0911@naver.com>
2023-07-10 15:51:01 +0900

    feat: 시스템 상수 선언

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,15 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    public static final int EXCEPTION_NUMBER = -1;
+    public static final int POBI_WINNER = 1;
+    public static final int CRONG_WINNER = 2;
+    public static final int NO_WINNER = 0;
+
+    public static final int LEFT_PAGE = 0;
+    public static final int RIGHT_PAGE = 1;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         System.out.println(pobi+ " "+ crong);
         int pobiRight = pobi.get(0);
