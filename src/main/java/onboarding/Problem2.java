4e0bf8680a058fae0c72578c6bade435652fe9c2
Jiyoon Kim <rlawldbs0911@naver.com>
2023-07-11 13:57:53 +0900

    [problem2] feat: cryptogram 유효성 검사

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,18 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        if (!checkValidationString(cryptogram)) {
+            throw new IllegalArgumentException();
+        }
+
         return answer;
+    }
+    private static boolean checkValidationString(String cryptogram) {
+        if (cryptogram.length() > 1000
+                || cryptogram.length() < 1
+                || !cryptogram.equals(cryptogram.toLowerCase())) {
+            throw new IllegalArgumentException();
+        }
+        return true;
     }
 }
