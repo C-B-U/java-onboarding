8752a4ed0e6a61b8aa2eeba12b7404a8fd7d7b9b
GaBaljaintheroom <junsu1222@naver.com>
2023-07-13 00:15:16 +0900

    feat: 페이지 입력 받기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,25 @@
 package onboarding;
 
+import javax.swing.text.html.Option;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.OptionalInt;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
-        return answer;
+        Stream<String> pobiPage = getPage(pobi);
+        Stream<String> crongPage = getPage(crong);
+
     }
+
+    private static Stream<String> getPage(List<Integer> user) {
+        return user.stream().map(Object::toString);
+    }
+
+
 }
