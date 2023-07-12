758296b169ef494f62b5db8a75119ff358a8d390
Jiyoon Kim <rlawldbs0911@naver.com>
2023-07-12 16:53:07 +0900

    (problem3) feat: 시스템 상수 선언

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,13 @@
 package onboarding;
 
 public class Problem3 {
+
+    public static final int MAXIMUM_NUMBER = 10000;
+    public static final int MINIMUM_NUMBER = 1;
+    public static final char TRHEE = '3';
+    public static final char SIX = '6';
+    public static final char NINE = '9';
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
