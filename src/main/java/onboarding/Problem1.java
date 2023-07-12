2dac8d326edfa94466ef405ab6a09e802e5e6a3c
GaBaljaintheroom <junsu1222@naver.com>
2023-07-13 02:02:04 +0900

    feat: 페이지 길이 에러 처리 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -11,11 +11,14 @@ class Problem1 {
     public static final int CRONG_WIN = 2;
     public static final int POBI_WIN = 1;
     public static final int NO_WINNER = 0;
-
+    public static final int isNotCorrectPage = -1;
 
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
 
+        if (isNotCorrectPage(pobi) || isNotCorrectPage(crong)) {
+            return isNotCorrectPage;
+        }
         int pobiMaxPage = getMaxPage(getPageSumMax(pobi), getPageMultiMax(pobi));
         int crongMaxPage = getMaxPage(getPageSumMax(crong), getPageMultiMax(crong));
 
@@ -49,7 +52,7 @@ class Problem1 {
     }
 
     private static int getWinner(int pobiMaxPage, int crongMaxPage) {
-        if (isPobiWin(pobiMaxPage,crongMaxPage))
+        if (isPobiWin(pobiMaxPage, crongMaxPage))
             return POBI_WIN;
 
         if (isCrongWin(pobiMaxPage, crongMaxPage))
@@ -58,31 +61,39 @@ class Problem1 {
         return NO_WINNER;
     }
 
-    private static boolean isPobiWin(int pobiMaxPage, int crongMaxPage){
-        if (pobiMaxPage > crongMaxPage){
+    private static boolean isPobiWin(int pobiMaxPage, int crongMaxPage) {
+        if (pobiMaxPage > crongMaxPage) {
             return true;
         }
         return false;
     }
 
-    private static boolean isCrongWin(int pobiMaxPage, int crongMaxPage){
-        if (crongMaxPage > pobiMaxPage){
+    private static boolean isCrongWin(int pobiMaxPage, int crongMaxPage) {
+        if (crongMaxPage > pobiMaxPage) {
             return true;
         }
         return false;
     }
 
-    private static boolean validateLeftRightPage(List<Integer> user){
-        if(user.get(0) != (user.get(1)+1)){
-            return false;
+    private static boolean validateLeftRightPage(List<Integer> user) {
+        if (user.get(0) == (user.get(1) - 1)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean validateStartOrEndPage(List<Integer> user) {
+        if (user.get(0) >= 1 && user.get(1) <= 400) {
+            return true;
         }
         return true;
     }
 
-    private static boolean validateStartOrEndPage(List<Integer> user){
-        if(user.get(0) < 1 && user.get(1) > 400){
-            return false;
+    private static boolean validatePageLength(List<Integer> user) {
+        if (user.size() == 2) {
+            return true;
         }
-        return true;
+        return false;
     }
+
 }
