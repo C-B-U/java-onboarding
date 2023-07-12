ec1a5cbd47ed89aa2066d8e0c8e1cbe7374ec30b
GaBaljaintheroom <junsu1222@naver.com>
2023-07-13 00:34:40 +0900

    feat: 페이지 최대값 메서드

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+import org.mockito.internal.matchers.Null;
+
 import javax.swing.text.html.Option;
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -9,32 +11,44 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+import static java.util.Collections.max;
+
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         Stream<String> pobiPage = getPage(pobi);
         Stream<String> crongPage = getPage(crong);
 
+        int pobiMaxPage = getMaxPage(getPageSumMax(pobiPage), getPageMultiMax(pobiPage));
+        int cringMaxPage = getMaxPage(getPageSumMax(crongPage), getPageMultiMax(crongPage));
+
 
     }
+
+
 
     private static Stream<String> getPage(List<Integer> user) {
         return user.stream().map(Object::toString);
     }
 
-    private static OptionalInt getPageSumMax(Stream<String> page) {
+    private static int getPageSumMax(Stream<String> page) {
         return page.mapToInt(str -> str.chars()
                         .map(Character::getNumericValue)
                         .sum())
-                .max();
+                .max().orElseThrow(NullPointerException::new);
     }
 
-    private static OptionalInt getPageMultiMax(Stream<String> page) {
+    private static int getPageMultiMax(Stream<String> page) {
         return page.mapToInt(str -> str.chars()
                         .map(Character::getNumericValue)
                         .reduce(1, (a, b) -> a * b))
-                .max();
+                .max().orElseThrow(NullPointerException::new);
     }
+
+    private static int getMaxPage(int pageSumMax, int pageMultiMax) {
+        return Math.max(pageSumMax, pageMultiMax);
+    }
+
 
 
 
