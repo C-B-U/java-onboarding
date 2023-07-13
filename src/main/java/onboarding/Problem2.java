975574aa2137eb20972445d6d6e5744d77f6c6a2
GaBaljaintheroom <junsu1222@naver.com>
2023-07-14 01:20:06 +0900

    feat: 중복된 문자 제거 메서드 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,20 +2,37 @@ package onboarding;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.Objects;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         Deque<Character> cryptogramStack = convertStringToStack(cryptogram);
-
+        Deque<Character> resultStack = removeDuplicateChar(cryptogramStack);
         return answer;
     }
+
     private static Deque<Character> convertStringToStack(String cryptogram){
         Deque<Character> stack = new ArrayDeque<Character>();
         for (char word : cryptogram.toCharArray()){
             stack.add(word);
         }
         return stack;
+    }
+
+    private static Deque<Character> removeDuplicateChar(Deque<Character> cryptogram) {
+        Deque<Character> resultStack = new ArrayDeque<Character>();
+        while (!cryptogram.isEmpty()){
+               Character word = cryptogram.removeLast();
+                // 결과 stack이 비어있지 않고 cryptogram의 마지막 요소와 word가 같으면(중복) 마지막 요소 제가
+               if (!resultStack.isEmpty() && Objects.equals(word, resultStack.peekLast())){
+                   resultStack.removeLast();
+                   continue;
+               }
+               resultStack.addLast(word);
+
+        }
+        return resultStack;
     }
 
 }
