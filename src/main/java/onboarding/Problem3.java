0106e2a23b3a04e66a05bd0e1fade0423e524eb0
GaBaljaintheroom <junsu1222@naver.com>
2023-07-15 00:20:47 +0900

    feat: 3,6,9횟수 반환하는 메소드 작성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,9 +4,8 @@ public class Problem3 {
 
     public static final String NUMBER_RANGE_ONE_TO_THOUSAND = "숫자의 범위는 1부터 10000 이하입니다.";
     public static int solution(int number) {
-        int answer = 0;
         validateNumberRange(number);
-        return answer;
+        return getCount(number);
     }
 
     private static void validateNumberRange(int number) {
@@ -14,4 +13,18 @@ public class Problem3 {
             throw new IllegalArgumentException(NUMBER_RANGE_ONE_TO_THOUSAND);
         }
     }
+
+    public static int getCount(int number){
+        int count = 0;
+        for (int i = 1; i <= number; i++){
+            String num = String.valueOf(i);
+            for (int j = 0; j < num.length(); j++){
+                if (num.charAt(j) == '3' || num.charAt(j) == '6' || num.charAt(j) == '9'){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
 }
