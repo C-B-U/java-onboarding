36e678312e2c16f1278e439fd652694e3d9933b8
GaBaljaintheroom <junsu1222@naver.com>
2023-07-15 00:11:29 +0900

    feat: 숫자 범위 에러처리 메소드 작성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,17 @@
 package onboarding;
 
 public class Problem3 {
+
+    public static final String NUMBER_RANGE_ONE_TO_THOUSAND = "숫자의 범위는 1부터 10000 이하입니다.";
     public static int solution(int number) {
         int answer = 0;
+        validateNumberRange(number);
         return answer;
+    }
+
+    private static void validateNumberRange(int number) {
+        if (number < 1 || number > 10000){
+            throw new IllegalArgumentException(NUMBER_RANGE_ONE_TO_THOUSAND);
+        }
     }
 }
