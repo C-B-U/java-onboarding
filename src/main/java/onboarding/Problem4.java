ac50d754feda9c1554f0edf8c53ac2a3cdfb486d
GaBaljaintheroom <junsu1222@naver.com>
2023-07-17 18:21:17 +0900

    feat: 알파벳 대소문자 변경 메소드 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,24 @@
 package onboarding;
 
+import onboarding.problem4.Alphabet;
+import static onboarding.problem4.Alphabet.mapAlphabet;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        Alphabet.getMapAlphabetToReverse();
+        return getResult(word);
+    }
+
+
+    public static boolean isLower(char ch){
+        return ch >= 'a' && ch <= 'z';
+    }
+
+    public static char toUpper(char ch){
+        return (char) (ch - 32);
+    }
+
+    public static char toLower(char ch){
+        return (char)(ch + 32);
     }
 }
--- src/main/java/onboarding/problem4/Alphabet.java
+++ src/main/java/onboarding/problem4/Alphabet.java
@@ -4,13 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public final class Alphabet {
-    private static final Map<Character, Character> mapAlphabet = new HashMap<>(26);
+    public static final Map<Character, Character> mapAlphabet = new HashMap<>(26);
 
-    public final Map<Character, Character> getMapAlphabetToReverse(){
+    public static void getMapAlphabetToReverse(){
        for(char c = 'A'; c <= 'Z'; c++){
            char reverseChar = (char)('Z' - (c - 'A'));
            mapAlphabet.put(c, reverseChar);
        }
-       return mapAlphabet;
     }
 }
