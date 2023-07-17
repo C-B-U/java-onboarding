1487fc5e5f0c14218840e1cdb4123c30d55ccc96
GaBaljaintheroom <junsu1222@naver.com>
2023-07-17 20:43:52 +0900

    feat: 알파벳 상수 추출

--- src/main/java/onboarding/problem4/Alphabet.java
+++ src/main/java/onboarding/problem4/Alphabet.java
@@ -3,12 +3,15 @@ package onboarding.problem4;
 import java.util.HashMap;
 import java.util.Map;
 
+import static onboarding.problem4.Sentence.END_UPPER_ALPHABET;
+import static onboarding.problem4.Sentence.START_UPPER_ALPHABET;
+
 public final class Alphabet {
     public static final Map<Character, Character> mapAlphabet = new HashMap<>(26);
 
     public static void getMapAlphabetToReverse(){
-       for(char c = 'A'; c <= 'Z'; c++){
-           char reverseChar = (char)('Z' - (c - 'A'));
+       for(char c = START_UPPER_ALPHABET; c <= END_UPPER_ALPHABET; c++){
+           char reverseChar = (char)(END_UPPER_ALPHABET - (c - START_UPPER_ALPHABET));
            mapAlphabet.put(c, reverseChar);
        }
     }
--- src/main/java/onboarding/problem4/Sentence.java
+++ src/main/java/onboarding/problem4/Sentence.java
@@ -4,6 +4,12 @@ import static onboarding.problem4.Alphabet.mapAlphabet;
 
 public class Sentence {
 
+    public final static Character START_LOWER_ALPHABET = 'a';
+    public final static Character END_LOWER_ALPHABET = 'z';
+    public final static Character START_UPPER_ALPHABET = 'A';
+    public final static Character END_UPPER_ALPHABET = 'Z';
+    public final static int CHANGE_ALPHABET_SIZE = 32;
+
     private final String sentence;
     private final StringBuilder sb = new StringBuilder();
 
@@ -29,23 +35,23 @@ public class Sentence {
     }
 
     private void whenLower(char ch){
-        if (ch >= 'a' && ch <= 'z'){
+        if (ch >= START_LOWER_ALPHABET && ch <= END_LOWER_ALPHABET){
             sb.append(toLower(mapAlphabet.get(toUpper(ch))));
         }
     }
 
     private void whenUpper(char ch){
-        if (ch >= 'A' && ch <= 'Z'){
+        if (ch >= START_UPPER_ALPHABET && ch <= END_UPPER_ALPHABET){
             sb.append(mapAlphabet.get(ch));
         }
     }
 
     private char toUpper(char ch){
-        return (char)(ch - 32);
+        return (char)(ch - CHANGE_ALPHABET_SIZE);
     }
 
     private char toLower(char ch){
-        return (char)(ch + 32);
+        return (char)(ch + CHANGE_ALPHABET_SIZE);
     }
 }
 
