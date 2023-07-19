29289bd0785361dc3418a3a975d44f39c34220a6
Jiyoon Kim <rlawldbs0911@naver.com>
2023-07-19 14:14:55 +0900

    (problem4) feat: 대소문자 아스키코드 상수 선언

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,10 @@
 package onboarding;
 
 public class Problem4 {
+
+    public static final int UPPER_CASE = 65;
+    public static final int LOWER_CASE = 97;
+
     public static String solution(String word) {
         String answer = "";
         return answer;
