62031e0f9f60fad6eeb0298bbc4e7c5132149167
Jiyoon Kim <rlawldbs0911@naver.com>
2023-07-21 14:39:08 +0900

    (problem5) feat: 메인 로직 메서드 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,12 +1,29 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem5 {
+
     public static List<Integer> solution(int money) {
         if (!checkValiation(money)) {
             throw new IllegalArgumentException();
+        }
+
+        int leftMoney = money;
+        int[] moneyArray = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        List<Integer> answer = new ArrayList<>();
+
+        for (int i = 0; i < moneyArray.length; i++) {
+            if(leftMoney < moneyArray[i]) {
+                answer.add(0);
+                continue;
+            }
+            answer.add(leftMoney/moneyArray[i]);
+            leftMoney = leftMoney % moneyArray[i];
         }
         return answer;
     }
