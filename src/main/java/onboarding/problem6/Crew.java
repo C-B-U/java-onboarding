b4d89ca39bf37cfda6f1dc0d20fbf25127a9111c
GaBaljaintheroom <junsu1222@naver.com>
2023-07-24 16:13:48 +0900

    refactor: Crew 클래스 수정

--- src/main/java/onboarding/problem6/Crew.java
+++ src/main/java/onboarding/problem6/Crew.java
@@ -1,23 +1,20 @@
 package onboarding.problem6;
 
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 public class Crew {
 
-    private final String email;
-    private final String name;
+    private String email;
+    private String name;
 
-    Crew(String email, String name){
+    Crew(String email, String name) {
         this.email = email;
         this.name = name;
     }
 
+    public String getName() {
+        return name;
+    }
 
-    List<Crew> crewsList (List<List<String>> forms) {
-        return forms.stream()
-                .map(form -> new Crew(form.get(0), form.get(1)))
-                .collect(Collectors.toList());
+    public String getEmail(){
+        return email;
     }
 }
