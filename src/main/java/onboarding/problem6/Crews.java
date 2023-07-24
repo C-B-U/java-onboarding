3b7e5ea4d1e48659e6363e03e79a29f3797fc8a7
GaBaljaintheroom <junsu1222@naver.com>
2023-07-24 16:14:34 +0900

    feat: Crews 객체 분리

---
+++ src/main/java/onboarding/problem6/Crews.java
@@ -0,0 +1,47 @@
+package onboarding.problem6;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Crews {
+
+    private List<Crew> crews;
+    private List<String> nicknames;
+
+    public Crews(List<List<String>> forms) {
+        this.crews = mapCrewsList(forms);
+        this.nicknames = mapNicknames();
+    }
+
+
+
+    public List<Crew> mapCrewsList(List<List<String>> forms) {
+        return forms.stream()
+                .map(form -> new Crew(form.get(0), form.get(1)))
+                .collect(Collectors.toList());
+    }
+
+    public List<String> mapNicknames(){
+       return crews.stream().map(Crew::getName).collect(Collectors.toList());
+    }
+
+
+    public List<String> getResultEmail() {
+        return nicknames.stream()
+                .filter(nickname -> {
+                    List<String> separated = new NameSeparate(nickname).getSeparatedNickname();
+                    return crews.stream()
+                            .map(Crew::getName)
+                            .filter(name -> !name.equals(nickname)) // Exclude the same nickname
+                            .anyMatch(name -> separated.stream().anyMatch(name::contains));
+                })
+                .flatMap(filterNickname -> crews.stream()
+                        .filter(crew -> filterNickname.equals(crew.getName()))
+                        .map(Crew::getEmail))
+                .sorted()
+                .distinct()
+                .collect(Collectors.toList());
+    }
+}
+
+
