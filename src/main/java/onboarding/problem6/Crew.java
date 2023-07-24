5b4923e6a7105cbcefba17166bcdfa1cd3dc6f5f
GaBaljaintheroom <junsu1222@naver.com>
2023-07-24 16:26:39 +0900

    refactor: 이메일 길이 에러처리 메서드

--- src/main/java/onboarding/problem6/Crew.java
+++ src/main/java/onboarding/problem6/Crew.java
@@ -1,5 +1,7 @@
 package onboarding.problem6;
 
+import java.util.regex.Pattern;
+
 public class Crew {
 
     private String email;
@@ -7,7 +9,11 @@ public class Crew {
 
     Crew(String email, String name) {
         validEmailType();
+        validEmailLength();
         this.email = email;
+
+        validNickNameLength();
+        validKoreanNickName();
         this.name = name;
     }
 
@@ -24,6 +30,14 @@ public class Crew {
             throw new IllegalArgumentException("이메일은 'email.com' 도메인만 가능합니다.");
         }
     }
+
+    private void validEmailLength() {
+        if (email.length() < 11 || email.length() >= 20){
+            throw new IllegalArgumentException("이메일 길이는 11자 이상 20자 미만입니다.");
+        }
+    }
+
+
 
 
 
